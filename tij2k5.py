c71c4117e7695fc1c19cd7963cd39faa18d6e2bb
Jun S <yfiua6@gmail.com>
2025-07-18 16:03:55 +0200

    extract k5s and their durations

--- tij2k5.py
+++ tij2k5.py
@@ -25,8 +25,8 @@ def main(f_input, f_output):
     min_t, max_t = df_tij['t'].min(), df_tij['t'].max()
     max_id = df_tij['j'].max() + 1
 
-    ## all tetrahedra
-    df_tetrahedra = pd.DataFrame(columns=['t', 'i', 'j', 'k', 'l'])
+    ## all K5s
+    df_k5s = pd.DataFrame(columns=['t', 'i', 'j', 'k', 'l', 'm'])
 
     ## function to get triangles, assuming i < j < k
     def get_triangles(A):
@@ -53,6 +53,18 @@ def main(f_input, f_output):
                 if k_ < l_:
                     yield i_, j_, k_, l_
 
+    # function to get K5s
+    def get_k5s(A):
+        # get tetrahedra first
+        i, j, k, l = zip(*get_tetrahedra(A))
+
+        for i_, j_, k_, l_ in zip(i, j, k, l):
+            # m is the fifth node that forms a K5 with i, j, k, l
+            m = np.where(A[i_, :] * A[j_, :] * A[k_, :] * A[l_, :])[0]
+            for m_ in m:
+                if l_ < m_:
+                    yield i_, j_, k_, l_, m_
+
     t_interval = 20
     for t in tqdm(np.arange(min_t, max_t, t_interval)):
         df_cursor = df_tij[np.logical_and(df_tij['t'] >= t, df_tij['t'] < (t + t_interval))]
@@ -61,17 +73,17 @@ def main(f_input, f_output):
         A = np.zeros([max_id, max_id])
         A[df_cursor['i'].values, df_cursor['j'].values] = 1
 
-        # tetrahedra
+        # K5s
         try:
-            i_, j_, k_, l_ = zip(*get_tetrahedra(A))
-            df_tetrahedra = pd.concat([df_tetrahedra, pd.DataFrame({'t': t, 'i': i_, 'j': j_, 'k': k_, 'l': l_})], ignore_index=True)
+            i_, j_, k_, l_, m_ = zip(*get_k5s(A))
+            df_k5s = pd.concat([df_k5s, pd.DataFrame({'t': t, 'i': i_, 'j': j_, 'k': k_, 'l': l_, 'm': m_})], ignore_index=True)
         except ValueError:
-            # if no tetrahedra found, continue
+            # if no K5 found, continue
             continue
 
     # Save data as CSV
-    df_tetrahedra = df_tetrahedra.astype(int)
-    df_tetrahedra.to_csv(f_output, index=False)
+    df_k5s = df_k5s.astype(int)
+    df_k5s.to_csv(f_output, index=False)
 
 if __name__ == '__main__':
     f_input = sys.argv[1]
--- tij2k5_duration.py
+++ tij2k5_duration.py
@@ -25,8 +25,9 @@ def main(f_input, f_output):
     min_t, max_t = df_tij['t'].min(), df_tij['t'].max()
     max_id = df_tij['j'].max() + 1
 
-    ## all tetrahedra
-    df_tetrahedra = pd.DataFrame(columns=['t', 'i', 'j', 'k', 'l'])
+    ## K5 age matrix
+    delta_t = {}
+    delta_t_history, i_history, j_history, k_history, l_history, m_history = [], [], [], [], [], []
 
     ## function to get triangles, assuming i < j < k
     def get_triangles(A):
@@ -53,6 +54,18 @@ def main(f_input, f_output):
                 if k_ < l_:
                     yield i_, j_, k_, l_
 
+    # function to get K5s
+    def get_k5s(A):
+        # get tetrahedra first
+        i, j, k, l = zip(*get_tetrahedra(A))
+
+        for i_, j_, k_, l_ in zip(i, j, k, l):
+            # m is the fifth node that forms a K5 with i, j, k, l
+            m = np.where(A[i_, :] * A[j_, :] * A[k_, :] * A[l_, :])[0]
+            for m_ in m:
+                if l_ < m_:
+                    yield i_, j_, k_, l_, m_
+
     t_interval = 20
     for t in tqdm(np.arange(min_t, max_t, t_interval)):
         df_cursor = df_tij[np.logical_and(df_tij['t'] >= t, df_tij['t'] < (t + t_interval))]
@@ -61,17 +74,42 @@ def main(f_input, f_output):
         A = np.zeros([max_id, max_id])
         A[df_cursor['i'].values, df_cursor['j'].values] = 1
 
-        # tetrahedra
+        # set for all K5s
         try:
-            i_, j_, k_, l_ = zip(*get_tetrahedra(A))
-            df_tetrahedra = pd.concat([df_tetrahedra, pd.DataFrame({'t': t, 'i': i_, 'j': j_, 'k': k_, 'l': l_})], ignore_index=True)
+            set_k5s = {(i, j, k, l, m) for i, j, k, l, m in get_k5s(A)}
         except ValueError:
-            # if no tetrahedra found, continue
-            continue
+            # if no K5 found, continue
+            set_k5s = set()
+
+        ## K5s to disappear, i.e., delta_t > 0 and not in set_tetrahedra
+        k5s_disappear = {k: delta_t[k] for k in delta_t.keys() if k not in set_k5s}
+
+        ## get indices for the k5s to disappear
+        if len(k5s_disappear) > 0:
+            i_disappear, j_disappear, k_disappear, l_disappear, m_disappear = zip(*k5s_disappear.keys())
+
+            ## record ages, i, j, k, l, and set ages as 0, for the edges to disappear
+            delta_t_history = np.append(delta_t_history, list(k5s_disappear.values()))
+            i_history = np.append(i_history, i_disappear)
+            j_history = np.append(j_history, j_disappear)
+            k_history = np.append(k_history, k_disappear)
+            l_history = np.append(l_history, l_disappear)
+            m_history = np.append(m_history, m_disappear)
+
+            # remove the k5s to disappear
+            for k in k5s_disappear.keys():
+                del delta_t[k]
+
+        ## update k5s age
+        for k in set_k5s:
+            if k in delta_t:
+                delta_t[k] += 1
+            else:
+                delta_t[k] = 1
 
     # Save data as CSV
-    df_tetrahedra = df_tetrahedra.astype(int)
-    df_tetrahedra.to_csv(f_output, index=False)
+    df_k5s = pd.DataFrame({'i': i_history, 'j': j_history, 'k': k_history, 'l': l_history, 'm': m_history, 'delta_t': delta_t_history}).astype(int)
+    df_k5s.to_csv(f_output, index=False)
 
 if __name__ == '__main__':
     f_input = sys.argv[1]
